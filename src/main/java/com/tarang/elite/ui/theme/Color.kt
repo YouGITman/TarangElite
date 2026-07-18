550b827c81eb381b9b764a29f302d694f4a1ec7f
YouGITman <learnfromxperts@gmail.com>
2026-07-18 23:22:48 +0100

    Forged 2.0

---
+++ src/main/java/com/tarang/elite/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.tarang.elite.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand
+val Amber400 = Color(0xFFDDB569)
+val Amber500 = Color(0xFFD4A547)
+val Amber200 = Color(0xFFEDDBB0)
+val AmberDeep = Color(0xFF3D2E0C)
+val OnAmber = Color(0xFF201804)
+
+// Base
+val Ink = Color(0xFF0F1E3D)
+val Surface1 = Color(0xFF16233F)
+val Surface2 = Color(0xFF1D2C4E)
+val Surface3 = Color(0xFF24345A)
+val OutlineSlate = Color(0xFF2E3F63)
+val TextPrimary = Color(0xFFEFF4FF)
+val TextSecondary = Color(0xFF9DB0CC)
+
+// Accents
+val Emerald = Color(0xFF34D399)
+val EmeraldDeep = Color(0xFF064E3B)
+val Cyan = Color(0xFF22D3EE)
+val CyanDeep = Color(0xFF155E75)
+val Violet = Color(0xFFA78BFA)
+val Rose = Color(0xFFFB7185)
+val RoseDeep = Color(0xFF6B1D2A)
+
+// Recovery gradient stops
+val ScoreLow = Color(0xFFF87171)
+val ScoreMid = Color(0xFFDDB569)
+val ScoreHigh = Color(0xFF34D399)
